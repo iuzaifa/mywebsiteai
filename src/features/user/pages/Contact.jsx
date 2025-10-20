b6d5ad695d9b5e904827ac82f31485af8b7103af
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-10-21 01:49:22 +0530

    update some code

--- src/features/user/pages/Contact.jsx
+++ src/features/user/pages/Contact.jsx
@@ -1,51 +1,48 @@
 import { Phone, Mail, MapPin } from "lucide-react";
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
     <>
-      <section className="max-w-7xl lg:mx-auto mx-4 w-full">
-        <div className="lg:flex justify-center gap-5">
-          <div className="rounded-sm p-5 bg-teal-500 my-3 lg:my-10">
-            <h3 className="flex items-center gap-2 text-2xl font-semibold text-white">
-              <Phone size={18} /> Phone{" "}
+      <section className="w-full px-5">
+        <div className="lg:flex justify-center gap-5 items-center">
+          <div className="flex flex-col justify-center h-32 w-80 w-md rounded-sm p-5 bg-teal-200/10 border-b-4 my-3 lg:my-10 border border-teal-500 dark:bg-slate-900 dark:border-slate-800">
+            <h3 className="pb-3 flex items-center gap-3 text-xl  font-semibold text-teal-500">
+              <Phone size={16} /> Phone{" "}
             </h3>
-            <Link className="text-sm text-white dark:text-slate-400 py-1">
+            <Link className="lg:text-sm text-xs text-teal-700 dark:text-slate-400 py-1">
               +91 6387869214
             </Link>
-            <Link className="text-sm text-white dark:text-slate-400 py-1">
+            <Link className="lg:text-sm text-xs text-teal-700 dark:text-slate-400 py-1">
               +91 9876543210
             </Link>
           </div>
-          <div className="rounded-sm p-5 bg-teal-500 my-3 lg:my-10">
-            <div className="contact-call">
-              <h3 className="flex justify-center items-center gap-2 text-2xl font-semibold text-teal-500">
-                <Mail size={18} /> Phone{" "}
-              </h3>
-              <Link className="text-sm text-slate-600 dark:text-slate-400 py-1">
-                +91 6387869214
-              </Link>
-              <Link className="text-sm text-slate-600 dark:text-slate-400 py-1">
-                +91 9876543210
-              </Link>
-            </div>
+
+          <div className="flex flex-col justify-center h-32 w-80 w-md rounded-sm p-5 bg-teal-200/10 border-b-4 my-3 lg:my-10 border border-teal-500 dark:bg-slate-900 dark:border-slate-800">
+            <h3 className="pb-3 flex items-center gap-3 text-xl  font-semibold text-teal-500">
+              <Mail size={17} /> Email{" "}
+            </h3>
+            <Link className="text-sm text-teal-700 dark:text-slate-400 pb-1">
+              abuhuzaifaw7@gmail.com
+            </Link>
+            <Link className="text-sm text-teal-700 dark:text-slate-400">
+              fazlurrahman345@gmail.com
+            </Link>
           </div>
 
-          <div className="rounded-sm p-5 bg-teal-500 my-3 lg:my-10">
-            <div className="contact-call">
-              <h3 className="flex justify-center items-center gap-2 text-2xl font-semibold text-teal-500">
-                <MapPin size={18} /> Phone{" "}
-              </h3>
-              <Link className="text-sm text-slate-600 dark:text-slate-400 py-1">
-                121 Rock Sreet, 21 Avenue, New York, NY 92103-9000
-              </Link>
-            </div>
+          <div className="flex flex-col justify-center h-32 w-80 rounded-sm p-5 bg-teal-200/10 border-b-4 my-3 lg:my-10 border border-teal-500 dark:bg-slate-900 dark:border-slate-800">
+            <h3 className="pb-3 flex items-center gap-3 text-xl  font-semibold text-teal-500">
+              <MapPin size={16} /> Hours{" "}
+            </h3>
+            <p className="text-sm text-teal-700 dark:text-slate-400 pb-1">
+              Mon – Fri …… 11 am – 8 pm, Sat, Sun …… 6 am – 8 pm
+            </p>
           </div>
         </div>
       </section>
     </>
   );
-}
+};
 
-export default Contact
+export default Contact;
