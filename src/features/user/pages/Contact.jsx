6d932b763ac6d79bd48fad9d9199618af6d6d922
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-10-21 20:29:22 +0530

    .....

--- src/features/user/pages/Contact.jsx
+++ src/features/user/pages/Contact.jsx
@@ -34,27 +34,8 @@ const Contact = () => {
         </div>
       </section>
 
-      <section className="max-w-6xl mx-auto bg-white dark:bg-slate-950/90 my-5 lg:my-14 p-5 lg:p-10 rounded-md border border-slate-300 dark:border-slate-800 shadow-sm">
-        <div className="lg:flex py-8 justify-center max-w-6xl mx-auto px-5 gap-4 lg:py-10 bg-slate-100/50 dark:bg-slate-800/50 rounded-sm">
-          <div className="lg:w-1/2 w-full  p-4">
-            <h3 className="text-5xl font-thin py-5 dark:text-slate-400">
-              Location{" "}
-            </h3>
-
-            <p className="text-slate-500 pb-3">
-              <strong>Address:</strong> 12, Kalindi Kunj Rd, Block B, Jamia
-              Nagar, Okhla, New Delhi, Delhi 110025
-            </p>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83922907926!2d77.06889943342536!3d28.5272803436318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce2e972e8a0b1%3A0xbcd6ea6f5c63e2e7!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1692269836550!5m2!1sen!2sin"
-              width="100%"
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              className="lg:h-80 h-32 border-teal-500 border-4"
-            />
-          </div>
-
+      <section className="lg:max-w-6xl mx-auto  dark:bg-slate-950/90 py-32 lg:my-14  lg:p-10  ">
+        <div className="lg:flex py-8 justify-center max-w-6xl mx-auto px-5 gap-10 lg:py-10 rounded-sm">
           <div className="lg:w-1/2 border lg:p-8 p-4 bg-white dark:bg-slate-950/90 dark:border-slate-600 rounded-md">
             <form action="">
               <div className="lg:flex gap-2 justify-center items-center my-2">
@@ -209,8 +190,31 @@ const Contact = () => {
               </div>
             </form>
           </div>
+         
+         
+          <div className="lg:w-1/2 w-full ">
+            <h3 className="text-5xl font-thin py-5 dark:text-slate-400">
+              Location
+            </h3>
+
+            <p className="text-slate-500 pb-3">
+              <strong>Address:</strong> 12, Kalindi Kunj Rd, Block B, Jamia
+              Nagar, Okhla, New Delhi, Delhi 110025
+            </p>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224345.83922907926!2d77.06889943342536!3d28.5272803436318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce2e972e8a0b1%3A0xbcd6ea6f5c63e2e7!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1692269836550!5m2!1sen!2sin"
+              width="100%"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              className="lg:h-80 h-32 border-teal-500 border-4"
+            />
+          </div>
         </div>
-        <div className="lg:flex justify-center gap-5 items-center">
+        
+        
+        
+        <div className="lg:flex justify-center gap-5 items-center m-3">
           <div className="flex flex-col justify-center lg:w-1/3 w-full  rounded-sm px-5 py-7 bg-white  hover:bg-white/70  border-b-4 my-3 lg:my-10 border border-slate-300 dark:bg-slate-900 dark:border-slate-800">
             <h3 className="pb-3 flex items-center gap-3 text-xl  font-semibold text-teal-500">
               <Phone size={16} /> Phone{" "}
