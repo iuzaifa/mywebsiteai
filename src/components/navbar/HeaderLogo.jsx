ed869be94122788b4b059f0713bae7a7fde5d1e5
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-10-24 19:23:09 +0530

    update

--- src/components/navbar/HeaderLogo.jsx
+++ src/components/navbar/HeaderLogo.jsx
@@ -5,8 +5,10 @@ const HeaderLogo = () => {
     <>
       {/* <img src="bg/logo.jpg" className="h-10 w-full pb-0 mb-0" alt="" /> */}
 
-      <div className=" bg-gradient-to-r ">
-        <h2 className="text-3xl font-semibold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600 px-2">
+      {/* <div className="px-2 rounded-full bg-gradient-to-r from-blue-600 to-purple-600"> */}
+      <div className="px-2 ">
+        <h2 className="text-3xl  italic font-semibold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600 px-2">
+          {/* <h2 className="text-3xl italic tracking-tight text-white"> */}
           asianarcs
         </h2>
       </div>
